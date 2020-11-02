cbbac37f665f54f7d53f63b8af631ea8a3bbadaf
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-02 22:06:07 +0100

    Added first template

--- products/views.py
+++ products/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def say_hi(request, name):
-    return HttpResponse('Hi ' + name)
+    params = {'name': name}
+    return render(request, 'say-hi.html', params)
