8d5390c24d2f937480d9bc7996055f6e27b798b0
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-10 14:11:18 +0100

    Database edits

--- products/models.py
+++ products/models.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 class Products(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
+    price = models.FloatField()
+    created_at = models.DateTimeField()
 
     def get_absolute_url(self):
         return reverse('product_details', args=(self.id,))
