4b0fe27c2093a258780b70e835832b9c0a1f575e
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-19 19:56:29 +0100

    Adding the userprofile

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,10 +6,16 @@ User = get_user_model()
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=256, help_text='Required, inform a valid adress.')
+    first_name = forms.CharField(max_length=30, 
+                                 required=False, 
+                                 help_text='Optional.')
+    last_name = forms.CharField(max_length=30, 
+                                required=False, 
+                                help_text='Optional.')
+    email = forms.EmailField(max_length=256, 
+                             help_text='Required, inform a valid adress.')
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', 'first_name', 'last_name', 'email')
+        fields = ('username', 'password1', 'password2', 'first_name',
+                  'last_name', 'email')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    address = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
