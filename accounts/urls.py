8a50a4e17a6100e371bd27e789e589348108a0a5
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-19 17:38:19 +0100

    Added authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-"""store URL Configuration
+"""products URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,20 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.shortcuts import redirect
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
-def home(request):
-    return redirect('products_list')
-
+from .views import signup
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('', include('products.urls')),
-    path('', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/signup/', signup, name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
