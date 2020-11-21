cdb3d6fe9e02dadd58a0a7cfd5bc31dc1cb069f6
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-22 00:05:19 +0100

    Created orders app

---
+++ orders/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from products.models import Product
+
+User = get_user_model()
+
+
+# Create your models here.
+class Order(models.Model):
+    address = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(auto_now_add=True)
