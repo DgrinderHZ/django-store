0b5f4cc4055fd9f20f592c6dd1eb2f5f67f7d1ea
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-25 19:00:09 +0100

    Format

--- orders/forms.py
+++ orders/forms.py
@@ -7,14 +7,14 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('address',)
-    
+
     def save(self, user):
         self.instance.user = user
         self.instance.save()
 
         for item in user.cart.items.all():
             self.instance.items.add(item)
-        
+
         user.cart.items.clear()
 
         return self.instance
