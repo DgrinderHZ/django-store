710f9703f414840622b36614978c86160fa9e8e5
DgrinderHZ <hassanzekkouri@gmail.com>
2020-11-26 20:35:16 +0100

    Add Gmail support

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -12,3 +12,11 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "iamxtra2020@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
